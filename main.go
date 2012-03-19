a360959e67b10d71f24e14373afb3489b9db25bb
= <david.e.shere@gmail.com>
2012-03-19 09:04:47 -0700

    changed from explicit print to a map function

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	l := life.New(80,24)
 	l.Init()
 	for generation := 0; ; generation++ {
+
 		l.Map(func(state bool) { 
 			var str string
 			if state { str = "\x1b[47m " } else { str = "\x1b[41m " }
